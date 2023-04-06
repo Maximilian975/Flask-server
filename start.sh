abfb09a60dac1ec3a5391e0f6c215d0ed6dfd373
Max <max.matttsson00@gmail.com>
2023-04-07 00:06:13 +0200

    hej

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 :
 sh stop.sh
 sleep 0.2
-nohup gunicorn --bind 0.0.0.0:5000 wsgi:application > logs.txt 2>&1 &
+nohup gunicorn --bind 0.0.0.0:5000 wsgi:app > logs.txt 2>&1 &
 PID=$!
 echo $PID > PID
 # tail -f logs.txt
