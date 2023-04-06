8d749ed47df68b59c8d4bb6fb579f19dbf770a6c
Max <max.matttsson00@gmail.com>
2023-04-07 00:05:21 +0200

    hej

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,3 @@
-from app import create_app
-import logging
-
-gunicorn_logger = logging.getLogger('gunicorn.error')
-
-application = create_app(logger_override=gunicorn_logger)
+from app import app
+if __name__ == "__main__":
+    app.run()
